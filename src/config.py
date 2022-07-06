60ed41217529e0af2539ab15f1dbe9d3b6425026
adityasbg <35772932+adityasbg@users.noreply.github.com>
2022-07-06 16:48:52 +0530

    added device

--- src/config.py
+++ src/config.py
@@ -4,9 +4,10 @@ MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
+DEVICE ="cuda"
 
-BERT_PATH = '../input/bert_base_uncased'
-MODEL_PATH = "model.bin"
+BERT_PATH = '/datasets/bert_base_uncased/'
+MODEL_PATH = "/datasets/bert_base_uncased/model.bin"
 TRAINING_FILE = '../input/imdb.csv'
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH, do_lower_case=True)
